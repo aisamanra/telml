c3f00e4963af7b59f6ae8f7d555506e689f2155b
Getty Ritter <gettylefou@gmail.com>
2015-05-10 17:54:30 -0700

    Added a lot of comments

--- Data/TeLML/Parser.hs
+++ Data/TeLML/Parser.hs
@@ -8,20 +8,39 @@ import Data.TeLML.Type
 type Result a = Either String (String, a)
 type Parse a = String -> Result a
 
+-- All of these characters are the ones which need escaping if used
+-- inside a document.
 isSpecial :: Char -> Bool
 isSpecial c = c `elem` "\\{}|"
 
+-- This is here for, uh, aesthetic reasons.
 throw :: a -> Either a b
 throw = Left
 
+-- This is 'fmap' named in such a way that it does not conflict with
+-- 'fmap'.
 over :: (a -> b) -> Result a -> Result b
 over _ (Left err)     = Left err
 over f (Right (s, x)) = Right (s, f x)
 
+{- And this is a monadic bind. You'll note that this basically has the
+ - same type as almost any other monadic parser combinator library, so
+ - you might be wondering why I'm not just using parsec. On the other
+ - hand, this is literally the only function I actually need there, so
+ - why have another dependency I need to keep up-to-date when I could
+ - just define this trivial function here?
+ -
+ - I'm also not defining this for `Monad` itself because then I'd have
+ - to deal with newtype wrappers and defining methods for the whole
+ - Functor=>Applicative=>Monad hierarchy, when all I really need is
+ - this.
+-}
 bind :: Result a -> ((String, a) -> Result b) -> Result b
 bind (Left err) _ = Left err
 bind (Right a)  f = f a
 
+-- Parse a text fragment, handling escapes. This will end as soon as it
+-- sees any non-escaped special character.
 pText :: Parse Fragment
 pText = over Text . go
   where go ('\\':x:xs)
@@ -31,6 +50,7 @@ pText = over Text . go
           | otherwise   = (x:) `over` go xs
         go "" = return ("", "")
 
+-- Parse a tag name of length >= 0.
 pTagName :: Parse String
 pTagName s = go s `bind` ensureLen
   where go i@(x:xs)
@@ -42,12 +62,15 @@ pTagName s = go s `bind` ensureLen
           | length name > 0 = return (xs, name)
           | otherwise       = throw "expected tag name after `\\'"
 
+-- Skip any space charaters, returning () for the first non-space
+-- character (including EOF).
 skipSpace :: Parse ()
 skipSpace i@(x:xs)
   | isSpace x = skipSpace xs
   | otherwise = return (i, ())
 skipSpace _ = return ("", ())
 
+-- Parse a tag assuming that a backslash has already been encountered.
 pTag :: Parse Fragment
 pTag i =
   bind (pTagName i) $ \ (i', name) ->
@@ -56,6 +79,8 @@ pTag i =
       ("",_)        -> throw "unexpected end-of-document while parsing tag"
       _             -> throw "expected start of block"
 
+-- Parse the vertical-bar-separated arguments to a tag, ending when a
+-- right curly brace is encountered.
 pArgs :: Parse [Document]
 pArgs ('}':xs) = return (xs, [])
 pArgs s = bind (pFragments s) $ \case
@@ -63,12 +88,14 @@ pArgs s = bind (pFragments s) $ \case
   ('}':xs, cs) -> return (xs, [cs])
   _            -> throw "[unreachable]"
 
+-- Parse any fragment, deciding whether to parse it as a tag or a text chunk
 pFragment :: Parse Fragment
 pFragment s@('\\':c:_)
   | isSpecial c     = pText s
 pFragment ('\\':xs) = pTag xs
 pFragment s         = pText s
 
+-- Parse multiple fragments, ending when it encounters a }, or |, or end-of-file.
 pFragments :: Parse Document
 pFragments "" = return ("", [])
 pFragments ('{':s) = bind (pFragments s) $ \case
@@ -81,6 +108,7 @@ pFragments s@(x:_)
       bind (pFragment s) $ \case
        (s', c) -> (c:) `over` pFragments s'
 
+-- | Parse a string into a @TeLML@ 'Fragment'.
 parse :: String -> Either String Document
 parse str = case pFragments str of
   Right ("", r)     -> return r
--- Data/TeLML/Type.hs
+++ Data/TeLML/Type.hs
@@ -7,7 +7,13 @@ import Data.Data (Data)
 import Data.Typeable (Typeable)
 import Data.String (IsString(..))
 
+-- | A 'Document' is zero or more 'Fragment's.
 type Document = [Fragment]
+
+-- | A 'Fragment' is either a snippet of text (as indicated by the
+--   'Text' constructor) or a tag (as indicated by the 'Tag'
+--   constructor). The former is a raw string, and the latter consists
+--   of a name followed by zero or more 'Document's.
 data Fragment
   = Text String
   | Tag String [Document]

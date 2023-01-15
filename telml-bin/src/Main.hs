3fc956d744be8d28d7e328428181b06fad0d3f12
Getty Ritter <tristero@infinitenegativeutility.com>
2023-01-14 17:46:04 -0800

    rearrange it all

--- telml-bin/src/Main.hs
+++ telml-bin/src/Main.hs
@@ -13,43 +13,117 @@ import qualified HsLua.Core as Lua
 import qualified System.Console.GetOpt as Opt
 import qualified System.Environment as Sys
 
-data Options = Options
-  { optInputFile :: Maybe String,
-    optUseDefaultTags :: Bool,
-    optTagFile :: Maybe String
-  }
-  deriving (Eq, Show)
+-- | The main driver
+main :: IO ()
+main = do
+  -- parse command-line options
+  options <- parseOpts
+  -- read the source file
+  telmlSource <- case optInputFile options of
+    Nothing -> getContents
+    Just f -> readFile f
+  -- attempt to parse it
+  let telml = case TeLML.parse telmlSource of
+        Right str -> str
+        Left err -> error err
+  -- read the Lua source file, if provided
+  luaSource <- case optTagFile options of
+    Nothing -> return ""
+    Just f -> BS.readFile f
+  -- run everything needed in the Lua context (i.e. evaluating the
+  -- source and then using it to interpret tags)
+  result <- Lua.runEither (luaMain options luaSource telml)
+  -- either print the result or print the error nicely
+  case result of
+    Right msg -> Text.putStr msg
+    Left err -> putStrLn (Exn.displayException err)
 
-opts :: [Opt.OptDescr (Options -> Options)]
-opts =
-  [ Opt.Option
-      ['n']
-      ["no-default-tags"]
-      (Opt.NoArg (\o -> o {optUseDefaultTags = False}))
-      "Do not include any default tags",
-    Opt.Option
-      ['t']
-      ["tags"]
-      (Opt.ReqArg (\f o -> o {optTagFile = Just f}) "[file]")
-      "The file of tag definitions to use"
-  ]
+-- * Lua stuff
 
-parseOpts :: IO Options
-parseOpts = do
-  args <- Sys.getArgs
-  let def =
-        Options
-          { optInputFile = Nothing,
-            optUseDefaultTags = True,
-            optTagFile = Nothing
-          }
-  case Opt.getOpt Opt.Permute opts args of
-    (flags, [], []) ->
-      return (foldl (flip id) def flags)
-    (flags, [input], []) ->
-      return (foldl (flip id) def flags) {optInputFile = Just input}
-    (_, _, errors) ->
-      error (unlines errors)
+-- | Everything in Lua should be in this monad, which includes our
+-- custom error type
+type LuaM r = Lua.LuaE Error r
+
+-- | Evaluate the provided Lua source code and then use it to
+-- interpret the `TeLML.Document`.
+luaMain :: Options -> BS.ByteString -> TeLML.Document -> LuaM Text.Text
+luaMain opts luaSource doc = do
+  -- load the basic libraries so we have access to stuff like `ipairs`
+  Lua.openbase
+  Lua.pop 1
+
+  -- create the global `telml` table as a namespace for tags
+  Lua.newtable
+  Lua.setglobal "telml"
+
+  -- evaluate the source file. (We don't care what it evaluates to.)
+  _ <- Lua.dostring luaSource
+
+  -- make sure that the user didn't do something funky like redefine
+  -- the global `telml` to a string.
+  telml <- Lua.getglobal "telml"
+  if telml /= Lua.TypeTable
+    then throw (RedefinedTable telml)
+    else return ()
+
+  -- walk over the document, evaluating as we go
+  handleDoc opts doc
+
+-- | Convert a `TeLML.Document` into a piece of `Text`
+handleDoc :: Options -> TeLML.Document -> LuaM Text.Text
+handleDoc opts = fmap mconcat . sequence . map (handleFrag opts)
+
+-- | Convert a `TeLML.Fragment` into a piece of `Text` with the
+-- relevant tag evaluation
+handleFrag :: Options -> TeLML.Fragment -> LuaM Text.Text
+handleFrag _ (TeLML.TextFrag text) = return text
+handleFrag opts (TeLML.TagFrag tag) = handleTag opts tag
+
+-- | Evaluate a tag in light of both the Lua source and the provided
+-- options
+handleTag :: Options -> TeLML.Tag -> LuaM Text.Text
+handleTag opts (TeLML.Tag n ps) = do
+  -- evaluate the "arguments" first
+  ps' <- mapM (handleDoc opts) ps
+  -- look up the tag in the table
+  Lua.pushstring (Text.encodeUtf8 n)
+  -- check the type of the thing we've gotten out. (If it wasn't
+  -- present in the table, we'll get `nil`.)
+  typ <- Lua.gettable 1
+  case typ of
+    Lua.TypeNil
+      -- Defer to the standard tags by default
+      | optUseDefaultTags opts -> standardTags n ps'
+      -- ...but if the user opted out, then throw errors
+      | otherwise -> throw (NoSuchTag n)
+    -- if it's a function, then we can call it!
+    Lua.TypeFunction -> do
+      -- it's already on the stack, so now we need to add all the
+      -- arguments to the stack. They're all strings, so push the
+      -- appropriate bytestrings there
+      mapM_ (Lua.pushstring . Text.encodeUtf8) ps'
+      -- Call the function with the number of args we've passed, and
+      -- expect a single return value
+      Lua.call (Lua.NumArgs (fromIntegral (length ps'))) 1
+      -- look at the top thing on the stack to make sure it's a string
+      -- (or convertible)
+      result <- Lua.tostring 2
+      case result of
+        -- if we got `Nothing`, then it's not a string; throw an error
+        Nothing -> do
+          actualtyp <- Lua.ltype 2
+          throw (NotAString n actualtyp)
+        -- otherwise, it's a string, so pass it back down!
+        Just r -> do
+          Lua.pop 1
+          return (Text.decodeUtf8 r)
+    -- if it's not `nil` _or_ a function, then produce an error about
+    -- it
+    _ -> throw (NotAFunction n typ)
+
+-- * Errors and error-handling
+
+-- We wrap the usual LuaHS error type in our own
 
 data Error
   = LuaError Lua.Exception
@@ -57,9 +131,15 @@ data Error
   deriving (Show)
 
 instance Exn.Exception Error where
+  -- did you know this tries to use `show` by default? I really wish
+  -- Haskell had something like the str/repr distinction or
+  -- debug/display distinction. Trying to force every string
+  -- representation into `show` is nonsense.
   displayException (LuaError err) = Exn.displayException err
   displayException (TeLMLError err) = Exn.displayException err
 
+-- We need this in order to use our custom error type as a wrapper
+-- around Lua ones
 instance Lua.LuaError Error where
   popException = do
     err <- Lua.changeErrorType Lua.popException
@@ -73,44 +153,15 @@ instance Lua.LuaError Error where
 
   luaException = LuaError . Lua.luaException
 
-type LuaM r = Lua.LuaE Error r
-
+-- | A function that makes it easy to throw our own exceptions
 throw :: Exn.Exception e => e -> LuaM a
 throw =
   Lua.liftIO . Exn.throwIO . TeLMLError . Exn.toException
 
-main :: IO ()
-main = do
-  options <- parseOpts
-  telmlSource <- case optInputFile options of
-    Nothing -> getContents
-    Just f -> readFile f
-  let telml = case TeLML.parse telmlSource of
-        Right str -> str
-        Left err -> error err
-  luaSource <- case optTagFile options of
-    Nothing -> return ""
-    Just f -> BS.readFile f
-  result <- Lua.runEither (luaMain luaSource telml)
-  case result of
-    Right msg -> Text.putStr msg
-    Left err -> putStrLn (Exn.displayException err)
-  return ()
+-- ** Custom errors
 
-luaMain :: BS.ByteString -> TeLML.Document -> LuaM Text.Text
-luaMain luaSource doc = do
-  Lua.openbase
-  Lua.pop 1
-
-  Lua.newtable
-  Lua.setglobal "telml"
-  _ <- Lua.dostring luaSource
-  telml <- Lua.getglobal "telml"
-  if telml /= Lua.TypeTable
-    then throw (RedefinedTable telml)
-    else return ()
-  handleDoc doc
-
+-- | This represents when a builtin tag expects a specific arity and
+-- we invoke it with the wrong one
 data BuiltinArityMismatch = BuiltinArityMismatch
   { bamExpected :: Int,
     bamProvided :: Int,
@@ -129,12 +180,15 @@ instance Exn.Exception BuiltinArityMismatch where
         show (bamProvided bam)
       ]
 
+-- | This is thrown when we can't find any tag with the relevant name
 data NoSuchTag = NoSuchTag {nstName :: Text.Text} deriving (Show)
 
 instance Exn.Exception NoSuchTag where
   displayException nst =
     "No such tag: `" ++ Text.unpack (nstName nst) ++ "`"
 
+-- | This is thrown when the Lua defined something with this name, but
+-- it wasn't actually a function we could call
 data NotAFunction = NotAFunction
   {nafName :: Text.Text, nafActual :: Lua.Type}
   deriving (Show)
@@ -149,6 +203,9 @@ instance Exn.Exception NotAFunction where
         " instead"
       ]
 
+-- | This is thrown when we call a Lua function and it returns
+-- something which either isn't a string or isn't trivially
+-- convertable to a string (like a number).
 data NotAString = NotAString
   {nasName :: Text.Text, nasActual :: Lua.Type}
   deriving (Show)
@@ -163,6 +220,8 @@ instance Exn.Exception NotAString where
         " instead"
       ]
 
+-- | This is thrown if the code for some reason tries to redefine
+-- `telml` into something that's not a table.
 data RedefinedTable = RedefinedTable
   {rtType :: Lua.Type}
   deriving (Show)
@@ -175,6 +234,7 @@ instance Exn.Exception RedefinedTable where
         " instead"
       ]
 
+-- | Print a Lua type nicely (for error message purposes)
 ppType :: Lua.Type -> String
 ppType Lua.TypeNil = "nil"
 ppType Lua.TypeBoolean = "boolean"
@@ -187,6 +247,10 @@ ppType Lua.TypeUserdata = "userdata"
 ppType Lua.TypeThread = "thread"
 ppType Lua.TypeNone = "something unspeakable"
 
+-- * Tag definitions
+
+-- | Try to interpret this tag from the standard set, throwing an
+-- error if it doesn't exist
 standardTags :: Text.Text -> [Text.Text] -> LuaM Text.Text
 standardTags n ps =
   case n of
@@ -224,30 +288,42 @@ simpleTag :: Text.Text -> [Text.Text] -> (Text.Text -> Text.Text) -> LuaM Text.T
 simpleTag _ [item] result = pure (result item)
 simpleTag name items _ = throw (BuiltinArityMismatch 1 (length items) name)
 
-handleDoc :: TeLML.Document -> LuaM Text.Text
-handleDoc = fmap mconcat . sequence . map handleFrag
+-- * Options and option-parsing
 
-handleFrag :: TeLML.Fragment -> LuaM Text.Text
-handleFrag (TeLML.TextFrag text) = return text
-handleFrag (TeLML.TagFrag tag) = handleTag tag
+data Options = Options
+  { optInputFile :: Maybe String,
+    optUseDefaultTags :: Bool,
+    optTagFile :: Maybe String
+  }
+  deriving (Eq, Show)
 
-handleTag :: TeLML.Tag -> LuaM Text.Text
-handleTag (TeLML.Tag n ps) = do
-  -- evaluate the "arguments" first
-  ps' <- mapM handleDoc ps
-  Lua.pushstring (Text.encodeUtf8 n)
-  typ <- Lua.gettable 1
-  case typ of
-    Lua.TypeNil -> standardTags n ps'
-    Lua.TypeFunction -> do
-      mapM_ (Lua.pushstring . Text.encodeUtf8) ps'
-      Lua.call (Lua.NumArgs (fromIntegral (length ps'))) 1
-      result <- Lua.tostring 2
-      case result of
-        Nothing -> do
-          actualtyp <- Lua.ltype 2
-          throw (NotAString n actualtyp)
-        Just r -> do
-          Lua.pop 1
-          return (Text.decodeUtf8 r)
-    _ -> throw (NotAFunction n typ)
+optionDescriptions :: [Opt.OptDescr (Options -> Options)]
+optionDescriptions =
+  [ Opt.Option
+      ['n']
+      ["no-default-tags"]
+      (Opt.NoArg (\o -> o {optUseDefaultTags = False}))
+      "Do not include any default tags",
+    Opt.Option
+      ['t']
+      ["tags"]
+      (Opt.ReqArg (\f o -> o {optTagFile = Just f}) "[file]")
+      "The file of tag definitions to use"
+  ]
+
+parseOpts :: IO Options
+parseOpts = do
+  args <- Sys.getArgs
+  let def =
+        Options
+          { optInputFile = Nothing,
+            optUseDefaultTags = True,
+            optTagFile = Nothing
+          }
+  case Opt.getOpt Opt.Permute optionDescriptions args of
+    (flags, [], []) ->
+      return (foldl (flip id) def flags)
+    (flags, [input], []) ->
+      return (foldl (flip id) def flags) {optInputFile = Just input}
+    (_, _, errors) ->
+      error (unlines errors)

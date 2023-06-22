b542b645cedf57e56cd9be3f606db2ac958520eb
Getty Ritter <tristero@infinitenegativeutility.com>
2023-06-21 23:08:18 -0700

    yeah whatever

--- telml-bin/test/TelmlBinTests.hs
+++ telml-bin/test/TelmlBinTests.hs
@@ -3,7 +3,7 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BS
-import Data.TeLML.Driver
+import Data.TeLML.Driver hiding (main)
 import qualified Data.Text as Text
 import Test.Hspec
 
